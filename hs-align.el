b218f2dff2925c464328c43363c8b140ad99d008
knupfer <fknupfer@gmail.com>
2014-09-10 14:05:44 +0200

    add docs and more patterns

--- hs-align.el
+++ hs-align.el
@@ -27,38 +27,78 @@
 
 (require 'haskell-mode)
 
-(defvar hs-align-operator-list '("=" "::" "--" "|"))
+(defvar hs-align-operator-list '(("=" 8) ("::" 8)
+				 ("--" 6) (">>=" 4)
+				 ("<-" 3) ("->" 3) ("=>" 3)
+				 ("==" 2) ("/=" 2)
+				 ("<=" 2) ("<" 2)
+				 (">=" 2) (">" 2)
+				 ("|" 2) ("." 2)
+				 ("/" 1) ("*" 1)
+				 ("+" 1) ("-" 1)
+				 ("(" 1) (")" 1)
+				 ("[" 1) ("]" 1)))
 
-(defun hs-align ()
-  (interactive)
+
+(define-minor-mode hs-align-mode
+  "Auto align operators and other keywords.
+
+The alignment is in the source file, so take care.  It checks
+lines consecutive lines, which contain point and an operator from
+`hs-align-operator-list' and aligns them if the maximal column
+difference is lower than the second argument in
+`hs-align-operator-list."
+  :init-value nil
+  :lighter " >>="
+  :global nil
+  (if hs-align-mode (add-hook 'after-change-functions 'hs-align-block nil t)
+    (remove-hook 'after-change-functions 'hs-align-block t)))
+
+(defun hs-align-block (a b c)
+  "Map the operators to the search.
+
+Vars A B and C are required by `after-change-functions' but
+thrown away."
   (mapc 'hs-align-search hs-align-operator-list))
 
-(defun hs-align-search (operator)
-  "Align operators in current block."
-  (setq op-list nil)
-  (save-excursion
-    (let (beg end (count 0) line-count (maxcol 0))
-      (setq line-count (line-number-at-pos))
-      (forward-line 0)
-      (re-search-backward (concat "^\\([^" operator
-				  "]\\|[^ ]" operator
-				  "\\|" operator
-				  "[^ ]\\)*$") nil t)
-      (setq line-count (line-number-at-pos))
-      (while (and (re-search-forward
-		   (concat "^.*?\\( +\\)"
-			   operator " .+$") nil t)
-		  (<= -1 (- line-count (setq line-count
-					     (line-number-at-pos)))))
-	(let ((posi (match-end 1))
-	      (leng (- (match-end 0) (match-beginning 0)))
-	      (space (- (match-end 1) (match-beginning 1)))
-	      (col (- (match-end 1) (match-beginning 0))))
-	  (setq op-list (cons (list posi leng space col) op-list)
-		maxcol (max maxcol (- col space -1)))))
-      (hs-align-insert op-list maxcol))))
+(defun hs-align-search (enum-operator)
+  "Align operators in current block.
+
+Receive a ENUM-OPERATOR list and split it in operators and
+distances."
+  (let ((operator (regexp-quote (car enum-operator)))
+	(operator-non-esc (car enum-operator))
+	(dist (cadr enum-operator))
+	(oplist nil))
+    (save-excursion
+      (let (beg end (count 0) line-count (maxcol 0) (mincol 72))
+	(setq line-count (line-number-at-pos))
+	(forward-line 0)
+	(re-search-backward (concat "^\\([^" operator-non-esc
+				    "]\\|[^ ]" operator
+				    "\\|" operator
+				    "[^ ]\\)*$") nil t)
+	(setq line-count (line-number-at-pos))
+	(while (and (re-search-forward
+		     (concat "^.*?\\( +\\)"
+			     operator " .+$") nil t)
+		    (<= -1 (- line-count (setq line-count
+					       (line-number-at-pos)))))
+	  (let ((posi (match-end 1))
+		(leng (- (match-end 0) (match-beginning 0)))
+		(space (- (match-end 1) (match-beginning 1)))
+		(col (- (match-end 1) (match-beginning 0))))
+	    (setq oplist (cons (list posi leng space col) oplist)
+		  maxcol (max maxcol (- col space -1))
+		  mincol (min mincol (- col space -1)))))
+	(when (and oplist (<=  (- maxcol mincol) dist))
+	  (hs-align-insert oplist maxcol))))))
 
 (defun hs-align-insert (oplist maxcol)
+  "Insert or remove whitespace.
+
+Retrieve from OPLIST pos of operator and compare it to MAXCOL,
+which represents the leftmost operator in this block."
   (while oplist
     (let ((elem (pop oplist)))
       (save-excursion
